c94d5a727245c26e27b3ff3679e52ca3e1f568b3
StLaurentMTL <lburton12@uchicago.edu>
2026-02-03 01:40:05 -0600

    Crawled for speeches ranging from 2022 - 2025

--- crawler.py
+++ crawler.py
@@ -1,57 +1,68 @@
-#习近平 talks crawler
+# 习近平 talks crawler
 
 import httpx
 import lxml.html
 import time
 import json
 import pprint
+import csv
 from pathlib import Path
 
-def crawler():
+SAVE_PATH = Path()
 
-    """"
-    Crawls for talks given by Xi Jin Ping
+speeches_filepath_json = SAVE_PATH / "speeches.json"
+speeches_filepath_csv = SAVE_PATH / "speeches.csv"
+
+HEADERS = ["publication", "title", "url", "text"]
+
+
+def crawler():
+    """ "
+    Crawls for public talks given by Xi Jinping published on state news agency
+    Xinhua from 2022 to 2026
 
     """
 
-    talks_repo = []
+    speeches_repo = []
 
-    url = "https://www.xinhuanet.com/politics/leaders/xijinping/jhqw.htm"
+    url = "https://www.xinhuanet.com/politics/leaders/xijinping/ds_3f92a99f87704592957a7478f07111c7.json"
 
     time.sleep(0.5)
     response = httpx.get(url)
+    speeches = response.json()
 
-    root = lxml.html.fromstring(response.text)
+    # Grabbing urls, speech titles, and date of publication
+    for speech in speeches["datasource"]:
+        speech_dict = {}
 
-    talks = root.cssselect("a")
+        speech_dict["url"] = speech["publishUrl"]
+        speech_dict["publication"] = speech["publishTime"]
+        speech_dict["title"] = (
+            speech["title"].split(">", 1)[1].rsplit("<", 1)[0].strip()
+        )
 
-    #Just grabbing the talks we want:
-    talks = talks[51:]
+        speeches_repo.append(speech_dict)
 
-    for talk in talks:
-
-        talk_dict = {}
-
-        talk_dict["Title"] = talk.text_content()
-        talk_dict["url"] = talk.get("href")
-        talks_repo.append(talk_dict)
-
-
-    for talk in talks_repo:
-
-        print(f"=========Crawling through {talk["url"]}=========")
-
-        talk_response = httpx.get(talk["url"])
-        talk_root = lxml.html.fromstring(talk_response.text)
-        talk_content = talk_root.cssselect("p")
+    # Grabbing text corpus
+    for speech in speeches_repo:
+        time.sleep(0.5)
+        speech_response = httpx.get(speech["url"])
+        speech_root = lxml.html.fromstring(speech_response.text)
+        speech_content = speech_root.cssselect("p")
 
         text_body = ""
 
-        for paragraph in talk_content:
-
+        for paragraph in speech_content:
             text_body += paragraph.text_content()
-        
-        talk["text"] = text_body
 
-        print(f"=========Talk: {talk["Title"]}===========")
-        pprint.pprint(talk)
+        speech["text"] = text_body
+
+    # JSON file save
+    with open(speeches_filepath_json, "w", encoding="utf-8") as json_file:
+        json.dump(speeches_repo, json_file, indent=1, ensure_ascii=False)
+
+    # CSV file save
+    with open(speeches_filepath_csv, "w", newline="", encoding="utf-8-sig") as f:
+        writer = csv.DictWriter(f, HEADERS)
+        writer.writeheader()
+        writer.writerows(speeches_repo)
