b34138bd4a029f1f757e055fe5253b8015220ad0
StLaurentMTL <lburton12@uchicago.edu>
2026-02-04 22:33:12 -0600

    added logic for Markov chain model

--- markov.py
+++ markov.py
@@ -1,25 +1,24 @@
 import math
 
+
 def k_tokens(text, k, idx):
-
     text_circular = text + text[0:k]
-    text_circular_plus = text + text[0:k + 1]
+    text_circular_plus = text + text[0 : k + 1]
 
-    return text_circular[idx:idx + k], text_circular_plus[idx:idx + k + 1]
+    return text_circular[idx : idx + k], text_circular_plus[idx : idx + k + 1]
+
 
 class Markov:
-
-    def __init__(self,k:int,text:str):
+    def __init__(self, k: int, text: str):
         """
         Constructs a new k-order markov model using provided text
         """
         self._text = text
-        self._k = k 
+        self._k = k
         self._tokens = {}
 
-        for idx,_ in enumerate(text):
-
-            k_string,k_string_plus = k_tokens(self._text,self._k,idx)
+        for idx, _ in enumerate(text):
+            k_string, k_string_plus = k_tokens(self._text, self._k, idx)
 
             if k_string not in self._tokens:
                 self._tokens[k_string] = 0
@@ -32,8 +31,8 @@ class Markov:
     def log_probability(self, text: str) -> float:
         """
         Retrieves the log, Laplace-smoothed probability of a given string given
-        the statistics of the character sequences model by the Markov model. 
-        
+        the statistics of the character sequences model by the Markov model.
+
         """
 
         # Intializing total likelihoods
@@ -42,8 +41,7 @@ class Markov:
         S = len(set(text))
 
         for idx, char in enumerate(text):
-            
-            k_string,k_string_plus = k_tokens(self._text,self._k,idx)
+            k_string, k_string_plus = k_tokens(self._text, self._k, idx)
 
             if self._tokens.get(k_string):
                 N = self._tokens.get(k_string)
@@ -54,13 +52,7 @@ class Markov:
                 M = self._tokens.get(k_string_plus)
             else:
                 M = 0
-            
+
             total_likelihood += math.log((M + 1) / (N + S))
 
         return total_likelihood
-            
-
-
-
-
-
