17ed34a0337ce4a7cf87d4dfd01771bb9bc5afc2
StLaurentMTL <lburton12@uchicago.edu>
2026-09-25 12:46:02 -0500

    Changed scope of project. Scraped text attribution exercise. Updated database

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
-# That's what Xi Said: A Computational Analysis of President Xi Jin Ping's (习近平) speeches
+# That's what Xi Said: A RAG Chatbot of President Xi Jin Ping's (习近平) speeches
 ## 习近平重要讲话计算文本分析
 
 ![Picture of President Xi Jinping's new year's address](https://github.com/user-attachments/assets/524e46b7-9786-4f4d-8145-6fb45727f1d2)
 
-"That's what Xi Said" is an ongoing textual analysis project of the public speeches of President Xi Jinping, the current head of state for the People's Republic of China. The project contains a convienient text corpus of Xi's public speeches from 2022-2025. 
+Based off of my work with the OECD, "That's what Xi Said" is an regularly updating Retrieval Augmented Generation
+(RAG) chatbot connected to a vectorized database of President Xi Jinping's speeches from 2022-2026. 
 
-Using this text corpus, I train a relatively simple k-th order Markov model that allows for probabilistic speaker attribution. While I have primarily adopted my Markov model algorithm from a previous project/assignment I wrote [^1], I am excited to see if these NLP techniques previously trained on an English language corpus will be salient for a Mandarin Chinese corpus. 
+This chatbot is intended for researchers, journalists, and chinawatchers to search, contrast, and
+compare Xi's public statements through the past decade. 
+This tool is not a replacement for proper and rigorous text analysis, but it is helpful for gaining insights and orienting researchers towards specific texts. 
 
-[^1]: A similar version of the model I wrote can be found here: https://www.cs.princeton.edu/courses/archive/spr08/cos226/assignments/markov.html
+
+
+
+
 
---
+++ processing/crawler.py
@@ -0,0 +1,75 @@
+# 习近平 talks crawler
+
+import httpx
+import lxml.html
+import time
+import json
+import pprint
+import csv
+from pathlib import Path
+
+SAVE_PATH = Path()
+
+speeches_filepath_json = SAVE_PATH / "data" / "speeches.json"
+speeches_filepath_csv = SAVE_PATH / "data" / "speeches.csv"
+
+HEADERS = ["publication", "title", "url", "text"]
+
+
+def crawler():
+    """ "
+    Crawls for public talks given by Xi Jinping published on state news agency
+    Xinhua from 2022 to 2026
+
+    """
+
+    speeches_repo = []
+
+    url = "https://www.xinhuanet.com/politics/leaders/xijinping/ds_3f92a99f87704592957a7478f07111c7.json"
+
+    time.sleep(0.5)
+    response = httpx.get(url)
+    speeches = response.json()
+
+    # Grabbing urls, speech titles, and date of publication
+    for speech in speeches["datasource"]:
+        speech_dict = {}
+
+        speech_dict["url"] = speech["publishUrl"]
+        speech_dict["publication"] = speech["publishTime"]
+        pprint.pprint(speech_dict["publication"])
+        speech_dict["title"] = (
+            speech["title"].split(">", 1)[1].rsplit("<", 1)[0].strip()
+        )
+
+        speeches_repo.append(speech_dict)
+
+    # Grabbing text corpus
+    for speech in speeches_repo:
+        time.sleep(1.0)
+        speech_response = httpx.get(speech["url"])
+        speech_root = lxml.html.fromstring(speech_response.text)
+        speech_content = speech_root.cssselect("p")
+
+        text_body = ""
+
+        for paragraph in speech_content:
+            text_body += paragraph.text_content()
+
+        speech["text"] = text_body
+
+        pprint.pprint(speech["text"])
+
+    # JSON file save
+    with open(speeches_filepath_json, "w", encoding="utf-8") as json_file:
+        json.dump(speeches_repo, json_file, indent=1, ensure_ascii=False)
+
+    # CSV file save
+    with open(speeches_filepath_csv, "w", newline="", encoding="utf-8-sig") as f:
+        writer = csv.DictWriter(f, HEADERS)
+        writer.writeheader()
+        writer.writerows(speeches_repo)
+
+
+# Running code
+crawler()
